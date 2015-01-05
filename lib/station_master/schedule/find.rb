c2c4fae8a041f0b67df26fd32f87343dbb1b1f7f
Stefano Ordine <stefano.ordine@gmail.com>
2015-01-05 17:49:33 +0100

    Add time zone constraint, Update gems dependencies

--- lib/station_master/schedule/find.rb
+++ lib/station_master/schedule/find.rb
@@ -3,14 +3,24 @@ module StationMaster
     module Find
       include Ask
 
-      def find_station_arrivals(station_code, time = Time.now)
-        MultiJson::load(remote_call(:arrivals, { station_code: station_code, time: format_time(time) }), symbolize_keys: true).inject([]) do |array, arrival_hash|
+      def find_station_arrivals(station_code, time = nil)
+        MultiJson::load(
+          remote_call(:arrivals, {
+            station_code: station_code,
+            time: format_time(time || Time.now.in_time_zone(TIME_ZONE)) }),
+          symbolize_keys: true)
+        .inject([]) do |array, arrival_hash|
           array << Arrival.new(arrival_hash)
         end
       end
 
-      def find_station_departures(station_code, time = Time.now)
-        MultiJson::load(remote_call(:departures, { station_code: station_code, time: format_time(time) }), symbolize_keys: true).inject([]) do |array, arrival_hash|
+      def find_station_departures(station_code, time = nil)
+        MultiJson::load(
+          remote_call(:departures, {
+            station_code: station_code,
+            time: format_time(time || Time.now.in_time_zone(TIME_ZONE)) }),
+          symbolize_keys: true)
+        .inject([]) do |array, arrival_hash|
           array << Departure.new(arrival_hash)
         end
       end
